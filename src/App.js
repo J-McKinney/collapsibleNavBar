a667854a28854d34fe7c2ade1e1cc38219c69363
Jesse McKinney <jessemckinney513@gmail.com>
2020-06-26 16:30:07 -0400

    got the navbar to disappear on scroll down and reappear on scroll up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "../src/NavBar/NavBar";
+import logo from "./logo.svg";
+import "./App.css";
+
+const navlinks = [
+  { name: "Home", to: "#/" },
+  { name: "About", to: "#/about" },
+  { name: "Contact", to: "#/contact" },
+];
+
+const brand = { name: "peekaboo", to: "/" };
 
 function App() {
   return (
     <div className="App">
+      <NavBar brand={brand} links={navlinks} />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
